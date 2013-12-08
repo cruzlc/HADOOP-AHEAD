a7e87d2125a35a547df0f16c6d9391fc82151032
Leonardo da Cruz <leonardocruz@gmail.com>
2013-12-08 00:33:14 -0200

    Atualiza run_AHEAD.sh

--- scripts/run_AHEAD.sh
+++ scripts/run_AHEAD.sh
@@ -4,6 +4,7 @@ function isNumeric () {
      echo "$var" | grep -q -v "[^0-9]"  && return 0 || return 1  
 }
 
+# Verifica validade do comando de entrada.
 if ( [[ "$1" != "-r" ]] || ( ! isNumeric "$2" ) || [[ "$2" == "" ]] || [[ "$3" != "-c" ]] || [[ ! -e "$4" ]] )
  then
   echo -e "\n"
@@ -12,6 +13,9 @@ if ( [[ "$1" != "-r" ]] || ( ! isNumeric "$2" ) || [[ "$2" == "" ]] || [[ "$3" !
   exit
 fi
 
+# Apaga dados (grafo) no HDFS.
 /opt/hadoop/bin/hadoop fs -rmr /user/hdfs/grafo
+# Coloca novos dados de entrada no HDFS (grafo).
 /opt/hadoop/bin/hadoop fs -put $4 /user/hdfs/grafo
+# Executa o AHEAD.
 time /opt/hadoop/bin/hadoop jar AHEAD.jar AHEAD.BFS_Paralelo $2 /user/hdfs/grafo
