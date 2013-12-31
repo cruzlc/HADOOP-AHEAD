2814814b419f65cb7e9e42a04dd0d1f58bc9fa23
Leonardo da Cruz <leonardocruz@gmail.com>
2013-12-31 12:39:00 -0200

    Update Scripts

--- scripts/build_AHEAD.sh
+++ scripts/build_AHEAD.sh
@@ -1,1 +1,14 @@
+# AHEAD - Advanced Hadoop Exact Algorithm for Distances
+# Programa desenvolvido para o projeto de pesquisa entitulado "Um Algoritmo Paralelo Eficiente para Cálculo da Centralidade em Grafos."
+
+# Referências:
+
+# CRUZ, L. C. ; MURTA, C. D. . Um Algoritmo Paralelo Eficiente para Cálculo da Centralidade em Grafos.
+# In: XIV Simpósio em Sistemas Computacionais (WSCAD-SSC), 2013, Porto de Galinhas PE. 
+# Anais do XIV Simpósio em Sistemas Computacionais (WSCAD-SSC). Porto Alegre: Sociedade Brasileira de Computação, 2013. p. 3-10.
+
+# Leonardo Carlos da Cruz. Um Algoritmo Paralelo Eficiente para Cálculo da Centralidade em Grafos. 2013. 
+# Dissertação (Mestrado em Modelagem Matemática e Computacional)
+# Centro Federal de Educação Tecnológica de Minas Gerais, . Orientador: Cristina Duarte Murta.
+
 ant -f build_AHEAD.xml makejar
